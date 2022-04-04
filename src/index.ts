f074e5b5d46ce22a2f396c993c028a7216b1ab2b
Muhaimin CS <muhaimincs@gmail.com>
2022-04-05 00:23:31 +0800

    add header control

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { handleRequest } from './handler'
+import { handleRequest, handleOptions } from './handler'
 
 addEventListener('fetch', (event: FetchEvent) => {
   try {
     const { request } = event
 
-    if (request.method.toUpperCase() !== 'GET') {
-      throw new Error('Unsupported method')
+    if (request.method.toUpperCase() === 'OPTIONS') {
+      // Handle CORS preflight requests
+      event.respondWith(handleOptions(request));
     }
   
-    // console.log('wok eleh', request)
     event.passThroughOnException();
-    return event.respondWith(handleRequest(event.request))
+    return event.respondWith(handleRequest(request))
   } catch (e: any) {
     return event.respondWith(
       new Response(e.message, {
